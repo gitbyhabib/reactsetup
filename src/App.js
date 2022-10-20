477d29069b700ae864017e7d1cbfb457414cf9f0
Md Habibur Rahman <typetohabib@gmail.com>
2022-10-20 17:40:18 +0600

    fixed bableeslint issues

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <h1>habibur Rahman</h1>
+    <h1>habibur Rahman</h1><h1>habibur Rahman</h1>
+    <h1>habibur Rahman</h1><h1>habibur Rahman</h1><h1>habibur Rahman</h1><h1>habibur Rahman</h1><h1>habibur Rahman</h1><h1>habibur Rahman</h1>
   );
 }
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
 
+const user = {
+  firstName: 'Harper',
+  lastName: 'Perez'
+};
+
+const element = (
+  <h1>
+    Hello, {formatName(user)}!
+  </h1>
+);
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
 import reportWebVitals from './reportWebVitals';
+
+const index = 0;
+
+function Clock({locale}) {
+ return (
+  <h1 className='heading' tabIndex={index}>
+    <span className='text'>Hello {new Date().toLocaleTimeString(locale)} </span>
+    <img src="" alt="" />
+  </h1>
+)
+}
+
+
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+function App() {
+  return (
+    <div>
+      <Welcome name="Sara" />
+      <Welcome name="Cahal" />
+      <Welcome name="Edite" />
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<React.StrictMode>
-    <App />
+  <React.StrictMode>
+    <Clock locale="bn-BD" />
+    
+    <Welcome/>
   </React.StrictMode>
 );
-
-
-
-
-
-
-
+  
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
