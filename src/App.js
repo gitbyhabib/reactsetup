c97db858bbea61e8e64114751ad905aec446b4de
Md Habibur Rahman <typetohabib@gmail.com>
2022-10-27 00:42:49 +0600

    new form control

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import Crender from "./components/Crender";
 
 import Clocklist from "./components/Clocklist";
+import Form from "./components/Form";
 function App() {
 
   const quantities = [1, 2, 3];
   
   console.log('app rended')
   return (
-    <div className="row m-0">
+    <div className="row m-0" >
       <div className="col-4">
         <Clocklist quantities={quantities}  />
       </div>
       <div className="col-8 text-center">
         <Crender/>
+        <Form/>
       </div>
     </div>
   );
